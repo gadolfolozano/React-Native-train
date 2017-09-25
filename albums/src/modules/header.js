9dce3323d0301165cd5d5bb75e7277fcd7c23876
AVANTICAPE\gustavo.lozano <gustavo.lozano@avantica.net>
2017-09-25 11:19:26 -0500

    aplying styles to header

--- albums/src/modules/header.js
+++ albums/src/modules/header.js
@@ -1,16 +1,30 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View} from 'react-native';
 
 const Header = () => {
 
-	const { textStyle } = styles;
+	const { textStyle, viewStyle } = styles;
 
 	return (
-		<Text style={textStyle}>Albums!</Text>
+		<View style={viewStyle}>
+			<Text style={textStyle}>Albums!</Text>
+		</View>
 	);
 };
 
 const styles = {
+	viewStyle: {
+		backgroundColor: '#f8f8f8',
+		justifyContent: 'center',
+		alignItems: 'center',
+		height: 60,
+		shadowColor: '#000000',
+		shadowOffset: { width: 0, height: 2 },
+		shadowOpacity: 0.2,
+		elevation: 2,
+		position: 'relative'
+	},
+	
 	textStyle: {
 		fontSize: 30
 	}

f2e48b53f51f45dc091c6ae2f49f79a1433ca13e
AVANTICAPE\gustavo.lozano <gustavo.lozano@avantica.net>
2017-09-25 17:56:59 -0500

    call service and make basic list

--- albums/src/modules/AlbumList.js
+++ albums/src/modules/AlbumList.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
-import {Text, View} from 'react-native'
+import {View} from 'react-native';
+import Axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component{
 
+	state = { albums: [] };
+
+	componentWillMount() {
+		Axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+			.then(response => this.setState({ albums: response.data }) );
+	}
+
+
+	renderAlbums() {
+		return this.state.albums.map((albumItem, index) => 
+			<AlbumDetail key={index} album={albumItem} />
+		);
+	}
+
 	render() {
+
+		console.log(this.state);
+
 		return(
 			<View>
-				<Text>Album List!!!</Text>
+				{this.renderAlbums()}
 			</View>
 		);
 	}	

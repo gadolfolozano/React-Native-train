284bdd0a63f5e8c48c504efb20fb3a79135c198e
AVANTICAPE\gustavo.lozano <gustavo.lozano@avantica.net>
2017-09-26 14:29:19 -0500

    add text and image to detail List

--- albums/src/modules/AlbumDetail.js
+++ albums/src/modules/AlbumDetail.js
@@ -1,20 +1,72 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
 import Card from './Card'; 
 import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({album}) => {
+
+	const { 
+		title, 
+		artist, 
+		image,
+		thumbnail_image,
+		thumbnailContainerStyle
+	} = album;
+	const { 
+		headerContentStyle,
+		thumbnailStyle, 
+		headerTextStyle,
+		imageStyle 
+	} = styles;
+
 	return (
 		<Card>
 			<CardSection>
-				<Text>{props.album.title}</Text>
-			</CardSection>		
-
+				<View style = {thumbnailContainerStyle}>
+					<Image
+						style = {thumbnailStyle}
+						source = {{ uri: thumbnail_image }}
+					/>
+				</View>
+				<View style = {headerContentStyle}>
+					<Text style = {headerTextStyle}>{title}</Text>
+					<Text>{artist}</Text>
+				</View>	
+			</CardSection>	
 			<CardSection>
-				<Text>{props.album.title}</Text>
+				<Image
+					style = {imageStyle}
+					source = {{ uri: image }}
+				/>
 			</CardSection>		
 		</Card>
 	);
 };
+
+
+const styles = {
+	headerContentStyle: {
+		flexDirection: 'column',
+		justifyContent: 'space-around'
+	},
+	headerTextStyle: {
+		fontSize: 18
+	},
+	thumbnailStyle: {
+		height: 50,
+		width: 50,
+		marginLeft: 10,
+		marginRight: 10
+	},
+	thumbnailContainerStyle: {
+		justifyContent: 'center',
+		alignItems: 'center'
+	}, 
+	imageStyle: {
+		height: 300,
+		flex: 1,
+		width: null
+	}
+}
 
 export default AlbumDetail;

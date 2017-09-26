d9a31c391438041a5edb8d7849a151b5c5938b26
AVANTICAPE\gustavo.lozano <gustavo.lozano@avantica.net>
2017-09-26 14:40:55 -0500

    adding scroll view parent

--- albums/index.android.js
+++ albums/index.android.js
@@ -5,7 +5,7 @@ import AlbumList from './src/modules/AlbumList';
 
 const App = () => {
   return(
-    <View>
+    <View style={{ flex: 1 }}>
       <Header headerText={'Albums!'}/>
       <AlbumList/>
     </View>

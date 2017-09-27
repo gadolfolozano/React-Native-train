5c270f64ea8246fe32f5763f6cbe6523436d38b5
AVANTICAPE\gustavo.lozano <gustavo.lozano@avantica.net>
2017-09-27 16:10:13 -0500

    Reusing components

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Header } from './components/common';
 
 class App extends Component {
   render() {
     return (
       <View>
-        <Text>Auth App</Text>
+        <Header headerText={'Auth App'}/>
+        <Text>detail</Text>
       </View>
     );
   }

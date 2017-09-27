88f05ea25a4e949e416b6c06dcb99a5f5879feef
AVANTICAPE\gustavo.lozano <gustavo.lozano@avantica.net>
2017-09-27 17:16:09 -0500

    setting up firebase library

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+
+  componentWillMount() {
+    firebase.initializeApp({
+    apiKey: "AIzaSyAvkBQnRmi0eDQkmlxN7DT1e5OilyxDhv0",
+    authDomain: "mi-salario-real.firebaseapp.com",
+    databaseURL: "https://mi-salario-real.firebaseio.com",
+    projectId: "mi-salario-real",
+    storageBucket: "mi-salario-real.appspot.com",
+    messagingSenderId: "805778536630"
+    });
+  }
+
   render() {
     return (
       <View>

a2efb8b9db3a7ccd72b5b3ab683c298b6e87283f
AVANTICAPE\gustavo.lozano <gustavo.lozano@avantica.net>
2017-10-06 17:06:04 -0500

    implementig touchEvent

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
 import { CardSection } from './common';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 
 class ListItem extends Component {
+
+  renderDetail(){
+    if( this.props.library.id === this.props.selectedLibraryId ) {
+      return (
+        <Text>
+          { this.props.library.description }
+        </Text>
+      );
+    }
+  }
+
   render() {
     const { textTitleStyle } = styles;
     console.log(this.props);
     return(
-      <CardSection>
-        <Text style={textTitleStyle} >
-          {this.props.library.title}
-        </Text>
-      </CardSection>
+      <TouchableWithoutFeedback
+        onPress={() => this.props.selectLibrary(this.props.library.id)}>
+        <View>
+          <CardSection>
+            <Text style={textTitleStyle} >
+              {this.props.library.title}
+            </Text>
+          </CardSection>
+          {this.renderDetail()}
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
@@ -25,4 +42,8 @@ const styles = {
   }
 };
 
-export default connect(null, actions)(ListItem);
+const mapStateToProps = state => {
+  return { selectedLibraryId: state.selectedLibraryId };
+}
+
+export default connect(mapStateToProps, actions)(ListItem);

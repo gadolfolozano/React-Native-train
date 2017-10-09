6343be06703eb4ca011c3eaac8c9b9cd1edcbfe8
AVANTICAPE\gustavo.lozano <gustavo.lozano@avantica.net>
2017-10-09 16:28:27 -0500

    add basic layout login

--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -1,11 +1,34 @@
 import React, {Component} from 'react';
-import { Card } from './common';
+import {
+  Card,
+  CardSection,
+  Input,
+  Button
+} from './common';
 
 class LoginForm extends Component {
 
   render() {
     return (
       <Card>
+        <CardSection>
+          <Input
+            placeholder = {'user@gmail.com'}
+            label = {'Email'}/>
+        </CardSection>
+
+        <CardSection>
+          <Input
+            secureTextEntry
+            placeholder = {'password'}
+            label = {'Password'}/>
+        </CardSection>
+
+        <CardSection>
+          <Button>
+            Login
+          </Button>
+        </CardSection>
       </Card>
     );
   }

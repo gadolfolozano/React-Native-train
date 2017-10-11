3fc85d06244a0c203523c35ebcc9f9466ba61bf1
AVANTICAPE\gustavo.lozano <gustavo.lozano@avantica.net>
2017-10-11 18:14:24 -0500

    add redux-thunk to firebase

--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
-import { emailChanged, passwordChanged } from '../actions';
+import { emailChanged, passwordChanged, loginUser } from '../actions';
 import {
   Card,
   CardSection,
@@ -16,6 +16,12 @@ class LoginForm extends Component {
 
   onPasswordChange(text) {
     this.props.passwordChanged(text);
+  }
+
+  onButtonPress() {
+    const { email, password } = this.props;
+
+    this.props.loginUser({ email, password });
   }
 
   render() {
@@ -40,7 +46,7 @@ class LoginForm extends Component {
         </CardSection>
 
         <CardSection>
-          <Button>
+          <Button onPress = {this.onButtonPress.bind(this)}>
             Login
           </Button>
         </CardSection>
@@ -56,4 +62,5 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps, { emailChanged, passwordChanged })(LoginForm);
+export default connect(mapStateToProps,
+  { emailChanged, passwordChanged, loginUser })(LoginForm);

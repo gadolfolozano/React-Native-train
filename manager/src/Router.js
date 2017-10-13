e84e9efbe2948d884cdbf273e93fe9450d606093
AVANTICAPE\gustavo.lozano <gustavo.lozano@avantica.net>
2017-10-13 16:24:59 -0500

    grouping scenes

--- manager/src/Router.js
+++ manager/src/Router.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Switch } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
 
 const RouterComponent = () => {
   return (
     <Router sceneStyle={{ paddingTop: 0 }}>
-      <Scene key="root">
-        <Scene key = "login" component = { LoginForm } title = "Please Login" />
+      <Scene key="root" hideNavBar>
+        <Scene key="auth" initial>
+          <Scene key = "login" component = { LoginForm } title = "Please Login"/>
+        </Scene>
+        <Scene key="main">
+          <Scene key = "employeeList" component = { EmployeeList } title = "Employees" />
+        </Scene>
       </Scene>
     </Router>
   );

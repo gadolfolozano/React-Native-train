b356ce8e731c230171b6be5fbe6be8dce1d577ea
AVANTICAPE\gustavo.lozano <gustavo.lozano@avantica.net>
2017-10-16 15:51:35 -0500

    making a form to create Employees

--- manager/src/components/EmployeeCreate.js
+++ manager/src/components/EmployeeCreate.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import {
+  Card,
+  CardSection,
+  Input,
+  Button,
+  Spinner
+} from './common';
 
 class EmployeeCreate extends Component {
   render () {
       return (
-        <View>
-          <Text>Employee Create</Text>
-        </View>
+        <Card>
+          <CardSection>
+            <Input
+              label="Name"
+              placeholder="Jane"
+            />
+          </CardSection>
+
+          <CardSection>
+            <Input
+              label="Phone"
+              placeholder="555-555-555"
+            />
+          </CardSection>
+
+          <CardSection>
+          </CardSection>
+
+          <CardSection>
+            <Button>
+              Create
+            </Button>
+          </CardSection>
+        </Card>
       );
   }
 }

b837137e6f56502bfd2efcc662395af63855b1b2
AVANTICAPE\gustavo.lozano <gustavo.lozano@avantica.net>
2017-10-16 17:54:21 -0500

    add picker redux

--- manager/src/components/EmployeeCreate.js
+++ manager/src/components/EmployeeCreate.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Picker } from 'react-native';
+import { connect } from 'react-redux';
+import { employeeUpdate } from '../actions';
 import {
   Card,
   CardSection,
@@ -16,6 +18,8 @@ class EmployeeCreate extends Component {
             <Input
               label="Name"
               placeholder="Jane"
+              value={this.props.name}
+              onChangeText={text => this.props.employeeUpdate({ prop: 'name', value: text})}
             />
           </CardSection>
 
@@ -23,10 +27,25 @@ class EmployeeCreate extends Component {
             <Input
               label="Phone"
               placeholder="555-555-555"
+              value={this.props.phone}
+              onChangeText={text => this.props.employeeUpdate({ prop: 'phone', value: text})}
             />
           </CardSection>
 
           <CardSection>
+            <Picker
+              style={{ flex: 1 }}
+              selectedValue={this.props.shift}
+              onValueChange={day => this.props.employeeUpdate({ prop: 'shift', value: day})}
+            >
+              <Picker.Item label="Monday" value="Monday"/>
+              <Picker.Item label="Tuesday" value="Tuesday"/>
+              <Picker.Item label="Wednesday" value="Wednesday"/>
+              <Picker.Item label="Thursday" value="Thursday"/>
+              <Picker.Item label="Friday" value="Friday"/>
+              <Picker.Item label="Saturday" value="Saturday"/>
+              <Picker.Item label="Sunday" value="Sunday"/>
+            </Picker>
           </CardSection>
 
           <CardSection>
@@ -39,4 +58,10 @@ class EmployeeCreate extends Component {
   }
 }
 
-export default EmployeeCreate
+const mapStateToProps = (state) => {
+  const { name, phone, shift } = state.employeeForm;
+
+  return {name, phone, shift};
+}
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
